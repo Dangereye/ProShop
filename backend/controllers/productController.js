e007970aa8fa9d312a771baa90b9d7fa96e83ad0
Craig Puxty <craig_puxty@talktalk.net>
2021-04-06 16:40:45 +0100

    Added Create & Update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,4 +30,56 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// Create a product - POST "/api/products" - (private/admin)
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    user: req.user._id,
+    name: "Sample name",
+    price: 0,
+    description: "Sample Description",
+    image: "/images/default.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// Update a product - PUT "/api/products/:id" - (private/admin)
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
